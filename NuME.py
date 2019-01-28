5ca03421d01927751b9b3bfab5563051da5fe233
Peng DING <pding.dp@gmail.com>
2019-01-28 00:04:06 -0600

    Reduce redundancy. Add keyboardInterrupt handle. Fix problem of "Desktop.ini"

--- NuME.py
+++ NuME.py
@@ -1,51 +1,62 @@
-import os
+import os, sys, traceback
 from tinytag import TinyTag
 _DEBUG = True
 
-pathToMusic = input("Pls specify the full path to music folder:")
-musicList = os.listdir(pathToMusic)
-exceptionList = []
-reserved = "<>:\"\\/|?*"
+def main():
+    pathToMusic = input("Pls specify the full path to music folder:")
+    musicList = os.listdir(pathToMusic)
 
-for each in musicList:
-    fileName, fileExtension = os.path.splitext(each)
-    eachPath = os.path.join(pathToMusic, each)
-    try:
-        tag = TinyTag.get(eachPath)
-        if (tag.title is None) or (tag.artist is None):
-            exceptionList.append(each)
-        else:
-            newName = '{} - {}'.format(tag.title, tag.artist)
-            newName = newName.replace('/', '.')
-            if newName != fileName:
-                newPath = newName + fileExtension
-                if not _DEBUG:
-                    os.rename(each, newPath, pathToMusic, pathToMusic)
-                else:
-                    print(newPath)
-    except:
-        exceptionList.append(each)
+    for each in musicList:
+        if each == 'desktop.ini':
+            continue
 
-print('''
-Now let\'s process failure cases
-''')
-
-for each in exceptionList:
-    fileName, fileExtension = os.path.splitext(each)
-    eachPath = os.path.join(pathToMusic, each)
-    try:
-        temp = fileName.replace('_', ' - ')
-        fileName = temp if temp != fileName else fileName
-        pre, post = fileName.split(' - ')
-        propose = post + ' - ' + pre
-        switch = input('''
-        Choose  1. {}
-                2. {}'''.format(fileName, propose))
-        if int(switch) == 2:
+        filename, fileExtension = os.path.splitext(each)
+        eachPath = os.path.join(pathToMusic, each)
+        try:
+            tag = TinyTag.get(eachPath)
+            if (tag.title is None) or (tag.artist is None):
+                propose = abnormal(filename)            
+            else:
+                propose = '{} - {}'.format(tag.title, tag.artist.replace('/', '.'))
+        except:
+                propose = abnormal(filename)
+        
+        if propose != filename:
             newPath = propose + fileExtension
+
             if not _DEBUG:
                 os.rename(each, newPath, pathToMusic, pathToMusic)
             else:
                 print(newPath)
+
+def abnormal(filename):
+    try:
+        temp = filename.replace('_', ' - ')
+        filename = temp if temp != filename else filename
+        
+        pre, post = filename.split(' - ')
+        propose = post + ' - ' + pre
+
+        switch = input('''
+        Choose  1. {}
+                2. {}
+                '''.format(filename, propose))
+        
+        if int(switch) == 1:
+            propose = filename
     except:
-        pass
+        propose = input("Unknown error! Pls check this filename: {}.".format(filename))
+    return propose
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+    except Exception:
+        traceback.print_exc(file=sys.stdout)
+    finally:
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
